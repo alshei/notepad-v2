748ad389357653231749a250df7125b3660ea352
alsheikhiu <alsheikh@iu.edu>
2022-01-07 00:42:09 -0500

    Update index.js

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { NotePencil, Trash } from "phosphor-react";
+import { NotePencil, Trash, CaretUp } from "phosphor-react";
 
 const Card = (props) => {
   return (
     <div>
-      <div className="w-full flex flex-row justify-between items-center rounded-t-lg p-6 bg-slate-900 drop-shadow-md text-white">
-        <p className="font-ptserif text-lg">{props.title}</p>
+      <button className="w-full flex flex-row justify-between gap-5 items-center rounded-t-lg p-6 bg-slate-900 hover:bg-slate-800 drop-shadow-md text-white">
+        <p className="font-ptserif text-lg text-left">{props.title}</p>
+
         <div className="flex flex-row items-center gap-5 justify-between">
           <a href={props.editlink}>
             <NotePencil size="1.5rem" />
@@ -14,7 +15,7 @@ const Card = (props) => {
             <Trash size="1.5rem" />
           </button>
         </div>
-      </div>
+      </button>
       <div className="w-full rounded-b-lg p-6 bg-mauve drop-shadow-md">
         <div class="inline-flex items-center justify-center px-2 py-1 text-xs font-semibold leading-none text-white bg-bubblegum rounded-full mb-6">
           {props.category}
