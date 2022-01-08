b886cf604afb424566b014a6b7ace7247a2a086f
alsheikhiu <alsheikh@iu.edu>
2022-01-07 23:28:25 -0500

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,10 +3,33 @@ const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, icon } = req.body;
-  res.json({
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists.");
+  }
+
+  const user = await User.create({
     name,
     email,
+    password,
+    icon,
   });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      icon: user.icon,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error has occured!");
+  }
 });
 
 module.exports = { registerUser };
