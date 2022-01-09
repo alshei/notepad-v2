714c7a15a3494b4c71b7fcda9783b2c2eae04b12
alsheikhiu <alsheikh@iu.edu>
2022-01-09 13:43:12 -0500

    Update index.js

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -4,19 +4,41 @@ import Form from "../../components/Form";
 import InputField from "../../components/InputField";
 import { Secondary } from "../../components/Buttons";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     console.log(email, password);
-  };
 
-  // useEffect(() => {
-  //   console.log(email, "<--email", password, "<--password");
-  // }, [email, password]);
+    try {
+      const config = {
+        header: {
+          "Content-type": "application/json",
+        },
+      };
+
+      setLoading(true);
+
+      const { data } = await axios.post(
+        "/api/users/login",
+        { email, password },
+        config
+      );
+
+      console.log(data);
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
 
   return (
     <div className="w-screen h-screen">
